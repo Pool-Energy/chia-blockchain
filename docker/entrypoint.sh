9578571ebd01ac339f83853f0bd0c2573870cf70
djerfy <djerfy@gmail.com>
2024-06-14 20:19:38 +0200

    Update chia-blockchain with pool.energy improvements
    Signed-off-by: djerfy <djerfy@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+
 set -e
 
-# Remove IPv6 for now
+# Update /etc/hosts in container (IPv6 removed)
 grep -v "::" /etc/hosts > /tmp/tmphosts
-# /etc/hosts cannot be moved/removed
 cat /tmp/tmphosts > /etc/hosts
 
 if [ ! -d "/data" ]; then
-	echo "No /data to persist"
+	echo "Error: no /data to persist"
 	exit 1
 fi
 
@@ -19,15 +19,18 @@ cd /root/chia-blockchain
 
 if [ ${chia_mode} = "wallet" ]; then
 
-	rm -rf /root/.chia
-	rm -rf /root/.chia_keys
+	rm -rf /root/.chia /root/.chia_keys
 	if echo ${network} | grep -q testnet; then
 		chia init --testnet
 	else
 		chia init
 	fi
 
-	chia keys add -l ${WALLET_ID} -f /data/wallet_keys_${WALLET_ID:=1}
+	# To use docker secret (require docker swarm orchestrator)
+	# echo "word1 word2 word3" | docker secret create wallet_keys_${WALLET_ID}
+	[ -e "/run/secrets/wallet_key_${WALLET_ID:=1}" ] && WALLET_KEY_PATH="/run/secrets"
+	chia keys add -l ${WALLET_ID} -f ${WALLET_KEY_PATH:=/data}/wallet_keys_${WALLET_ID:=1}
+
 	rm -rf /root/.chia/mainnet/wallet
 	mkdir -p /data/wallet_${WALLET_ID:=1}
 	ln -fs /data/wallet_${WALLET_ID:=1} /root/.chia/mainnet/wallet
@@ -40,7 +43,8 @@ if [ ${chia_mode} = "wallet" ]; then
 		cp -a /root/.chia/mainnet/config/ssl/* /data/wallet_${WALLET_ID:=1}_ssl/
 	fi
 
-	python3 /root/change_config.py
+	python3 /root/update-config.py
+	rm -rf /root/update-config.py
 
 	./venv/bin/python -m chia.daemon.server &
 	while ! nc -z -w 1 localhost 55400; do
@@ -49,10 +53,15 @@ if [ ${chia_mode} = "wallet" ]; then
 	done
 
 	if [ -n "${CHIA_EXPORTER}" ]; then
-		/root/chia_exporter/chia_exporter &
+		/root/chia-exporter/chia_exporter serve &
+	fi
+
+	if [ -n "${CHIA_STDOUT}" ]; then
+		tail -F /root/.chia/mainnet/log/debug.log &
 	fi
 
 	exec ./venv/bin/chia_wallet
+
 else
 
 	mkdir -p /root/.chia
@@ -66,6 +75,8 @@ else
 
 	sed -i 's/self_hostname:.*/self_hostname: \&self_hostname 0.0.0.0/' /data/chia/${network}/config/config.yaml || true
 
+	rm -rf /root/update-config.py
+
 	./venv/bin/python -m chia.daemon.server &
 	while ! nc -z -w 1 localhost 55400; do
 		echo "waiting 55400"
@@ -73,7 +84,11 @@ else
 	done
 
 	if [ -n "${CHIA_EXPORTER}" ]; then
-		/root/chia_exporter/chia_exporter &
+		/root/chia-exporter/chia_exporter serve &
+	fi
+
+	if [ -n "${CHIA_STDOUT}" ]; then
+		tail -F /root/.chia/mainnet/log/debug.log &
 	fi
 
 	exec ./venv/bin/chia_full_node
