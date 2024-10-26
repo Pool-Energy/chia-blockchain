776f0d1c9ab01328bf1055faeab42c89cf80b98b
djerfy <djerfy@gmail.com>
2024-10-26 18:54:54 +0200

    Add push_transaction endpoint to wallet RPC client
    Signed-off-by: djerfy <djerfy@gmail.com>

--- chia/rpc/wallet_rpc_api.py
+++ chia/rpc/wallet_rpc_api.py
@@ -182,6 +182,7 @@ class WalletRpcApi:
             "/send_transaction_multi": self.send_transaction_multi,
             "/spend_clawback_coins": self.spend_clawback_coins,
             "/get_coin_records": self.get_coin_records,
+            "/push_transaction": self.push_transaction,
             "/get_farmed_amount": self.get_farmed_amount,
             "/create_signed_transaction": self.create_signed_transaction,
             "/delete_unconfirmed_transactions": self.delete_unconfirmed_transactions,
@@ -1272,6 +1273,21 @@ class WalletRpcApi:
             "transaction_ids": [tx.name.hex() for tx in tx_list if tx.type == TransactionType.OUTGOING_CLAWBACK.value],
             "transactions": [tx.to_json_dict_convenience(self.service.config) for tx in tx_list],
         }
+
+    async def push_transaction(self, request: Dict[str, Any]) -> EndpointResult:
+        assert self.service.wallet_state_manager is not None
+
+        if await self.service.wallet_state_manager.synced() is False:
+            raise ValueError("Wallet needs to be fully synced before pushing transactions")
+
+        wallet_id = uint32(request["wallet_id"])
+        wallet = self.service.wallet_state_manager.wallets[wallet_id]
+        tx = TransactionRecord.from_json_dict(request["transaction"])
+
+        async with self.service.wallet_state_manager.lock:
+            await wallet.push_transaction(tx)
+
+        return {"success": True}
 
     async def delete_unconfirmed_transactions(self, request: Dict[str, Any]) -> EndpointResult:
         wallet_id = uint32(request["wallet_id"])
